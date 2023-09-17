a0a5143f11c5875e6bba8d21f620d2b0011ec2df
rahul kumar <me.rahulkumar@icloud.com>
2023-09-17 20:56:11 +0530

    build: loading component whihc render while api is being fetch added loading component to job listing page

--- client/src/job/JobListing.js
+++ client/src/job/JobListing.js
@@ -2,16 +2,23 @@ import React, { useContext, useEffect, useState } from 'react';
 import JobCard from './JobCard';
 import './job.css';
 import { JobsContext } from './Context';
+import Loading from '../components/Loading';
 
 export default function JobPage() {
   const { jobs } = useContext(JobsContext);
   const [jobData, setJobData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       try {
         const data = await jobs; // Wait for the Promise to resolve
         setJobData(Object.values(data));
+        
+        setTimeout(()=>{
+          setIsLoading(false);
+        },250);
       } catch (error) {
         console.error('Error fetching jobs:', error);
       }
@@ -20,30 +27,32 @@ export default function JobPage() {
     fetchData();
   }, [jobs]);
 
-  
+
   return (
-    <div className='joblisting'>
+    <>
+      {isLoading ? <Loading /> :
+        <div className='joblisting'>
+          {
+            !jobData || jobData.length === 0 ? (
+              <div className="no-content">
+                <p>No Jobs Available</p>
+              </div>
+            ) :
+              jobData && jobData.map((job) => (
+                <JobCard
 
-      {
-        !jobData || jobData.length === 0 ? (
-          <div className="no-content">
-            <p>No Jobs Available</p>
-          </div>
-        ) :
-        jobData && jobData.map((job) => (
-        <JobCard
-
-        key={job.id}
-        id={job.id}
-        title={job.title} 
-        company_id={job.company_id} 
-        requirement={job.requirement}
-        skills={job.requirement} 
-        description = {job.description}
-        />
-      ))
-    }
-    </div>
-
+                  key={job.id}
+                  id={job.id}
+                  title={job.title}
+                  company_id={job.company_id}
+                  requirement={job.requirement}
+                  skills={job.requirement}
+                  description={job.description}
+                />
+              ))
+          }
+        </div>
+      }
+    </>
   );
 }
