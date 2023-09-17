3189d7f9a71a76e645645e1148a3a064e15f0c31
rahul kumar <me.rahulkumar@icloud.com>
2023-09-17 20:19:02 +0530

    fix: labels of Industries

--- server/src/main/java/com/example/jobPortal/utils/Industry.java
+++ server/src/main/java/com/example/jobPortal/utils/Industry.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Industry {
-        AGRICULTURE("Agriculture, Forestry, Fishing and Hunting"),
-        MINING("Mining, Quarrying, and Oil and Gas Extraction"),
+        AGRICULTURE("Agriculture Forestry Fishing and Hunting"),
+        MINING("Mining Quarrying and Oil and Gas Extraction"),
         CONSTRUCTION("Construction"),
         MANUFACTURING("Manufacturing"),
         WHOLESALE_TRADE("Wholesale Trade"),
@@ -21,7 +21,7 @@ public enum Industry {
         ADMINISTRATIVE_SUPPORT("Administrative and Support Services"),
         EDUCATION("Educational Services"),
         HEALTH_CARE("Health Care and Social Assistance"),
-        ARTS("Arts, Entertainment, and Recreation"),
+        ARTS("Arts Entertainment and Recreation"),
         ACCOMMODATION_FOOD_SERVICES("Accommodation and Food Services"),
         OTHER("Other");
 
